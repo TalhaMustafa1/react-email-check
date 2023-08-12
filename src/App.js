aa9a21b35887e11fc627d75ed8dc0c04d31a8e3f
Talha Mustafa <91594421+TalhaMustafa1@users.noreply.github.com>
2023-08-12 06:32:18 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    // Function to get URL parameter value by name
+    const getParameterValue = (name) => {
+      const params = new URLSearchParams(window.location.search);
+      return params.get(name);
+    };
+
+    // Get the 'email' parameter from the URL
+    const emailFromUrl = getParameterValue('email');
+
+    // Set the email state if the parameter is present
+    if (emailFromUrl) {
+      setEmail(emailFromUrl);
+
+      // Remove the 'email' parameter from the URL
+      const urlWithoutEmail = window.location.href.replace(`email=${emailFromUrl}`, '');
+      window.history.replaceState({}, document.title, urlWithoutEmail);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-container">
+      <div className="left-part">
+        <h1 className="text-4xl">Left Part</h1>
+      </div>
+      <div className="right-part">
+        <h1 className="text-4xl">Right part</h1>
+        <input type="email" value={email} readOnly />
+      </div>
     </div>
   );
 }
